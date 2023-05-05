491c4d48e8f2c592d0495e7cb1aa1b34b8265d96
MuhHanif <48muhhanif@gmail.com>
2023-05-05 10:18:11 +0700

    queue producer

--- gradio_module/modules.py
+++ gradio_module/modules.py
@@ -2,6 +2,8 @@ import urllib.request
 import os
 import requests
 from typing import Any
+import pika
+import json
 
 
 def download_file(url: str, folder: str) -> None:
@@ -47,3 +49,42 @@ def upload_file(url: str, file_path: str) -> Any:
         files = {"file": f}
         response = requests.post(url, files=files)
     return response
+
+
+def put_message_in_queue(json_config: str, queue_name: str, message: str) -> None:
+    """
+    Put a message into a RabbitMQ queue.
+
+    Args:
+        json_config: Config credential file
+        queue_name: The name of the queue to put the message into.
+        message: The message to put into the queue.
+
+    Returns:
+        None
+    """
+    json_config = os.path.join(os.getcwd(), json_config)
+
+    with open(json_config, "r") as conf:
+        config = json.load(conf)
+
+    # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+    url = os.environ.get(
+        "CLOUDAMQP_URL",
+        config["credential"],
+    )
+    params = pika.URLParameters(url)
+
+    # Set up connection parameters
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+
+    # Declare a queue
+    channel.queue_declare(queue=queue_name)
+
+    message = json.dumps(message)
+    # Publish the message to the queue
+    channel.basic_publish(exchange="", routing_key=queue_name, body=message)
+
+    # Close the connection to RabbitMQ server
+    connection.close()
