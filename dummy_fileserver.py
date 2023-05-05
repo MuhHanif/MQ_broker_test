d8d21a23e6bb3f70d41debff3d62dd061d2b8f23
MuhHanif <48muhhanif@gmail.com>
2023-05-05 08:36:34 +0700

    simple server

--- dummy_fileserver.py
+++ dummy_fileserver.py
@@ -15,7 +15,7 @@ async def test_connection(name: str):
     return {"message": f"Hello, {name}!"}
 
 
-@app.post("/upload/audio_test_upload/")
+@app.post("/upload/audio/")
 async def upload_audio_file_test_upload(file: UploadFile = File(...)) -> dict:
     """
     Endpoint that accepts an audio file and saves it to a specified folder.
@@ -27,7 +27,7 @@ async def upload_audio_file_test_upload(file: UploadFile = File(...)) -> dict:
         dict: A JSON object containing the filename of the uploaded file.
     """
     # Specify the folder where the file will be saved
-    upload_folder = os.getcwd() + "/audio"
+    upload_folder = os.getcwd() + "/dummy_file_server/audio"
 
     # Create the folder if it doesn't exist
     os.makedirs(upload_folder, exist_ok=True)
@@ -48,7 +48,43 @@ async def upload_audio_file_test_upload(file: UploadFile = File(...)) -> dict:
     # contents: bytes = await file.read()
     # audio_queue.put(contents)
 
-    return {"message": "working as intended"}
+    return {"message": "audio uploaded"}
+
+
+@app.post("/upload/vtt/")
+async def upload_audio_file_test_upload(file: UploadFile = File(...)) -> dict:
+    """
+    Endpoint that accepts an audio file and saves it to a specified folder.
+
+    Args:
+        file (UploadFile): The audio file to upload.
+
+    Returns:
+        dict: A JSON object containing the filename of the uploaded file.
+    """
+    # Specify the folder where the file will be saved
+    upload_folder = os.getcwd() + "/dummy_file_server/vtt"
+
+    # Create the folder if it doesn't exist
+    os.makedirs(upload_folder, exist_ok=True)
+
+    # Check if the uploaded file is an audio file
+    valid_extensions = [".vtt", ".srt"]
+    file_extension = os.path.splitext(file.filename)[1].lower()
+
+    if file_extension not in valid_extensions:
+        raise HTTPException(status_code=400, detail="File must be a vtt file")
+
+    # Save the file to the specified folder
+    file_path = os.path.join(upload_folder, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    # im disabling using queue for now
+    # contents: bytes = await file.read()
+    # audio_queue.put(contents)
+
+    return {"message": "text transcription uploaded"}
 
 
 uvicorn.run(app, port=8000, host="0.0.0.0")
