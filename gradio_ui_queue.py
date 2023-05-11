a4c7f6d8c68afec0749082cae9f247cf54189073
MuhHanif <48muhhanif@gmail.com>
2023-05-12 01:48:03 +0700

    polished

--- gradio_ui_queue.py
+++ gradio_ui_queue.py
@@ -23,63 +23,76 @@ with open(json_config, "r") as conf:
 
 
 # function that executed in the web ui
-def read_binary_file(file) -> str:
-    # =============[send both image and message to dummy server]============= #
-    with open(json_config, "r") as conf:
-        config = json.load(conf)
+def read_binary_file(file, test_convert: bool = False) -> str:
+    try:
+        # =============[send both image and message to dummy server]============= #
+        with open(json_config, "r") as conf:
+            config = json.load(conf)
 
-    upload_folder = os.path.join(os.getcwd(), "upload")  # TODO: hoist this!
+        upload_folder = os.path.join(os.getcwd(), "upload")  # TODO: hoist this!
 
-    # Create the folder if it doesn't exist
-    os.makedirs(upload_folder, exist_ok=True)
+        # Create the folder if it doesn't exist
+        os.makedirs(upload_folder, exist_ok=True)
 
-    file_name = file.name.replace(" ", "_")
-    os.rename(file.name, file_name)
-    print(file_name)
+        file_name = file.name.replace(" ", "_")
+        os.rename(file.name, file_name)
+        print(file_name)
 
-    if file_name.split(".")[-1] != "mp3":
-        # converted audio file path
-        saved_file = os.path.join(
-            upload_folder, file_name.split("/")[-1].split(".")[0] + ".mp3"
-        )
+        print_out = "MP3 detected, skipping conversion"
+        if file_name.split(".")[-1] != "mp3":
+            # converted audio file path
+            saved_file = os.path.join(
+                upload_folder, file_name.split("/")[-1].split(".")[0] + ".mp3"
+            )
 
-        # execute ffmpeg bash script
-        ffmpeg_convert = ["ffmpeg", "-i", file_name, saved_file]
-        print_out = subprocess.run(
-            ffmpeg_convert, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+            # execute ffmpeg bash script
+            ffmpeg_convert = ["ffmpeg", "-i", file_name, saved_file, "-y"]
+            print_out = subprocess.run(
+                " ".join(ffmpeg_convert),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+            )
+            if not test_convert:
+                response = upload_file(url=url, file_path=saved_file)
+        else:
+            if not test_convert:
+                saved_file = file_name
+                response = upload_file(url=url, file_path=file_name)
 
-        response = upload_file(url=url, file_path=saved_file)
-    else:
-        saved_file = file_name
-        response = upload_file(url=url, file_path=file_name)
+        if not test_convert:
+            # dummy payload format, only changing file URI for now
+            payload = {
+                "company_id": "63446d348baa6c988b337b22",
+                "file_name": saved_file.split("/")[-1],
+                "file_uri": bucket_url,
+                "model": "medium",
+                "priority": "normal",
+                # replace request uuid with incrementing value
+                "request_uuid": "6408351c1fee0ed8f06c0716",
+                "speaker1": "channel1",
+                "speaker2": "channel2",
+            }
 
-    # dummy payload format, only changing file URI for now
-    payload = {
-        "company_id": "63446d348baa6c988b337b22",
-        "file_name": saved_file.split("/")[-1],
-        "file_uri": bucket_url,
-        "model": "medium",
-        "priority": "normal",
-        # replace request uuid with incrementing value
-        "request_uuid": "6408351c1fee0ed8f06c0716",
-        "speaker1": "channel1",
-        "speaker2": "channel2",
-    }
+            payload["file_uri"] = payload["file_uri"] + saved_file.split("/")[-1]
+            # payload = json.dumps(payload)
+            # enqueue message indicating that the audio file is uploaded
+            output = None
+            if response.status_code == 200:
+                put_message_in_queue(
+                    json_config="conf.json", queue_name=config["queue"], message=payload
+                )
+                output = response.text
+            else:
+                output = "upload failed"
 
-    payload["file_uri"] = payload["file_uri"] + saved_file.split("/")[-1]
-    # payload = json.dumps(payload)
-    # enqueue message indicating that the audio file is uploaded
-    output = None
-    if response.status_code == 200:
-        put_message_in_queue(
-            json_config="conf.json", queue_name=config["queue"], message=payload
-        )
-        output = response.text
-    else:
-        output = "upload failed"
+        else:
+            output = print_out
 
-    return output  # + str(binary_data)
+        return output  # + str(binary_data)
+
+    except Exception as e:
+        return f"ERROR: {e}"
 
 
 def retrieve_message():
@@ -108,13 +121,17 @@ with demo:
         with gr.TabItem("upload audio"):
             with gr.Row():
                 # file input
-                input_file = gr.File()
+                input_file = gr.File(label="drop your audio files here")
             with gr.Row():
                 # prompted output if failed or succeed
                 text_output2 = gr.Textbox()
 
             # execute function
-            upload = gr.Button("send to queue")
+            with gr.Row():
+                test_gradio_file_conversion = gr.Checkbox(
+                    label="test audio conversion to MP3"
+                )
+                upload = gr.Button("send to queue")
 
         # download vtt files from queue
         with gr.TabItem("download vtt"):
@@ -122,7 +139,11 @@ with demo:
                 image_output = gr.TextArea()
             retrieve = gr.Button("retrieve queue")
 
-    upload.click(read_binary_file, inputs=input_file, outputs=[text_output2])
+    upload.click(
+        read_binary_file,
+        inputs=[input_file, test_gradio_file_conversion],
+        outputs=[text_output2],
+    )
     retrieve.click(retrieve_message, inputs=None, outputs=image_output)
 
 demo.launch(server_name="0.0.0.0", server_port=9789)
